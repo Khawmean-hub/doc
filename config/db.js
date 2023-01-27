4ca50e06ef546d8c94502aa25ca33eb2b2795b62
Khaw Mean <khawmean8@gmail.com>
2023-01-27 08:05:16 +0700

    ok

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgres://ioatdkop:Jwxkjpj0unTI5BZ7cEvZromZTjNPrF3O@arjuna.db.elephantsql.com/ioatdkop')
+const db = pgp('postgresql://stdy:stdy@192.168.178.83:5432/stdydb')
 // const db = pgp('postgres://postgres:123@localhost:5432/b2b')
 db.one('SELECT $1 AS value', 123)
   .then((data) => {
@@ -8,6 +8,5 @@ db.one('SELECT $1 AS value', 123)
   .catch((error) => {
     console.log('ERROR:', error)
   })
-
 
 module.exports = db;
--- routes/home.js
+++ routes/home.js
@@ -31,12 +31,21 @@ homeRoute.post('/doc_menu_home_r01', async (req, res, next) => {
 
 
 homeRoute.post('/doc_department_r001', async (req, res, next) => {
-   var data = await db.any(`select dep_id, dep_name from doc_department where dep_status = 1 order by dep_id`);
-   if(!isNull(data))
-        return res.send(new BaseRes(true, "Success", data))
-   else
+    defaultPageInput(req);
+
+
+    var count = await db.one('select count(*) from doc_department where dep_status = 1');
+    var page = new Paging(req.body.OFFSET, req.body.LIMIT, count.count);
+
+
+    var resp = await db.any(`select dep_id, dep_name from doc_department where dep_status = 1 order by dep_id
+    LIMIT ${page.limit} OFFSET ${page.getOffset()};`);
+
+    if (resp == null) {
         return res.send(new BaseRes(false, "Error", null))
+}
 })
+
 
 homeRoute.get('/doc_menu_r01', async (req, res, next) => {
     var acticle = await db.any(`SELECT a.id, a.tag_id, a.title
@@ -93,5 +102,28 @@ homeRoute.post('/doc_file_r01', async (req, res, next) => {
 
 
 // testing
+homeRoute.post('/doc_menu_r02', async (req, res, next) => { //http://localhost:3000/doc_menu_r02?id=222
+
+    var acticle = await db.any(`SELECT a.id, a.tag_id, a.file_article_id, t.title as tag_title,a.title, a.content_body, 
+    a.modified_date , a.status,b.username,a.user_id,
+    f.file_id,f.file_idnt_id,f.file_nm,f.file_size,f.thum_img_path,f.img_path
+    FROM stdy.doc_articles a 
+    inner join doc_users b on a.user_id = b.id 
+    left join doc_tags t on t.id = a.tag_id  
+    left join doc_file as f on f.file_article_id = a.file_article_id
+    where a.id=cast($1 as INTEGER) and a.status = 1 and t.status = 1`, [req.body.ID]);
+    if (acticle == null ) {
+        return res.send(new BaseRes(false, "Error", null))
+    } else {
+        var ress = [];
+        if(acticle.length>0){
+            ress=acticle[0];
+        }
+        res.send(new BaseRes(true, "Success", ress))
+    }
+
+
+    
+})
 
 module.exports = homeRoute;
