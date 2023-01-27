24f966926cf2972a598b530caf420657c62de0a0
Khaw Mean <khawmean8@gmail.com>
2023-01-27 13:24:47 +0700

    done

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 const pgp = require('pg-promise')(/* options */)
-const db = pgp('postgresql://stdy:stdy@192.168.178.83:5432/stdydb')
+//const db = pgp('postgresql://stdy:stdy@192.168.178.83:5432/stdydb')
+const db = pgp('postgresql://postgres:123@172.104.182.46:5432/postgres')
 // const db = pgp('postgres://postgres:123@localhost:5432/b2b')
 db.one('SELECT $1 AS value', 123)
   .then((data) => {
