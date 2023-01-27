819e165ba8b773218065126ec0460be26bc48969
Khaw Mean <khawmean8@gmail.com>
2023-01-27 14:31:24 +0700

    okK

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const pgp = require('pg-promise')(/* options */)
 //const db = pgp('postgresql://stdy:stdy@192.168.178.83:5432/stdydb')
-const db = pgp('postgresql://postgres:123@172.104.182.46:5432/postgres')
+//const db = pgp('postgresql://postgres:123@172.104.182.46:5432/postgres')
+const db = pgp('postgresql://ioatdkop:Jwxkjpj0unTI5BZ7cEvZromZTjNPrF3O@arjuna.db.elephantsql.com:5432/ioatdkop')
 // const db = pgp('postgres://postgres:123@localhost:5432/b2b')
 db.one('SELECT $1 AS value', 123)
   .then((data) => {
