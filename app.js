5f5a7f7c613d9b22956d810b4c7ac88844fc381e
Khaw Mean <khawmean8@gmail.com>
2023-01-31 16:20:09 +0700

    ok

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 
-var allowedOrigins = ['http://127.0.0.1:5500'];
+var allowedOrigins = ['http://127.0.0.1:5500', 'http://127.0.0.1:5501'];
 app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin 
