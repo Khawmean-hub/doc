393e4ab657dd97496701dc0619411e35b66c6c66
Khaw Mean <khawmean8@gmail.com>
2023-02-06 16:39:00 +0700

    ok

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,12 @@
 FROM node:16.13.0-alpine
-
 WORKDIR /app
-
 COPY package.json ./
 COPY package-lock.json ./
-
 RUN npm config set unsafe-perm true
 RUN npm install --silent
-
+RUN mkdir imgs
 COPY . .
-
-RUN chown -R node /app/node_modules
-
-USER node
+RUN chown -R root /app/node_modules
+USER root
 EXPOSE 3000
 CMD ["node", "app.js"]
--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 
-var allowedOrigins = ['http://127.0.0.1:5500', 'http://127.0.0.1:5501'];
+var allowedOrigins = ['http://127.0.0.1:5500', 'http://172.104.160.141:5501'];
 app.use(cors({
   origin: function(origin, callback){
     // allow requests with no origin 
@@ -27,7 +27,7 @@ app.use(cors({
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json());
-// app.use('/image', express.static(process.env.IMG_PATH));
+app.use('/image', express.static(process.env.IMG_PATH));
 
 // Routes
 app.use(require('./routes/home'))
